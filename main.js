1a6e11817e4056fb842a1ed3c755504815be16e4
seanrenton.sr <seanrenton.sr@gmail.com>
2016-12-15 19:04:54 -0500

    Pair programming restaurant

--- main.js
+++ main.js
@@ -34,4 +34,69 @@ var AmericanFood = new Fooditem({
   vegan: "no",
   glutenfree: "no",
   citrusfree: "no",
-})                        
+})     
+
+class Drinks {
+  constructor(name, description, price, ingredients) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.ingredients = ingredients;
+   }
+}
+  
+  var Coconutrum = new Drinks(
+  "Coconut Rum",
+  "alcoholic beverage",
+  "9.99",
+  ["malibu rum", "coca cola", "splash of pineapple juice"]
+);
+
+class Plates {
+  constructor(name, description, price, ingredients) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.ingredients = ingredients;
+  }
+  
+}
+   
+var SmotheredEnchilads = new Plates (
+   "Smothered Green Chile Enchiladas",
+   "Beef, Chicken, Steak",
+   12.99,
+   ["choice of meat", "corn tortilla", "green chile", "cheese"]
+)
+
+
+
+var tostada = new Plates(
+  'Tostada',
+  "Flour tortilla grilled with deliciousness inside",
+   10.99,
+  ["Flour Tortilla, Refried Beans, Lettuce, Salsa, Sour Cream"]
+)
+
+var HuevosRancheros = new Plates(
+  'HuevosRancheros',
+   "Smothered eggs with hashbrowns",
+   14.99,
+   ["Organic Cage Free Eggs", "Salsa", "Ranch", "Hash Browns"]
+)
+
+var Burrito = new Plates(
+   'Burrito',
+   "Tortilla filled with goodness",
+   10.99,
+   ["Refried Beans", "Choice of: Steak, Chicken, or Beef", "Cheese"]
+)
+
+
+
+
+
+
+
+
+
